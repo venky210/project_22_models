bb078d5cf1ff2edcb9c7b9ff1992f61c6a554fc5
venky210 <vitsl.20735a0210@gmail.com>
2023-12-19 15:21:12 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from app.models import *
+from django.http import HttpResponse
+from django.db.models.functions import Length
+
+#DISPLAY THE DATA
+
+def display_topic(request):
+    QLTO=Topic.objects.all()
+    d={'topic':QLTO}    
+    return render(request,'display_topic.html',d)
+
+def display_webpage(request):
+    QLWO=Webpage.objects.all()
+    d={'webpage':QLWO}
+    return render(request,'display_webpage.html',d)
+    
+
+def display_accessrecord(request):
+    QLAO=AccessRecord.objects.all()
+    d={'accessrecord':QLAO}
+    return render(request,'display_accessrecord.html',d)
+
