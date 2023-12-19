6ed82c42548d0fcae02c638b9beaf1ff5fcaf1a3
venky210 <vitsl.20735a0210@gmail.com>
2023-12-19 17:50:32 +0530

    first commit

--- project_22/urls.py
+++ project_22/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('display_topic/',display_topic,name='display_topic'),
     path('display_webpage/',display_webpage,name='display_webpage'),
     path('display_accessrecord/',display_accessrecord,name='display_accessrecord'),
-    
 ]
